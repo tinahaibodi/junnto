55b4d7c8a943fe5e7873b37a8c457d06e639f53e
srafi1 <shakilrafi0@gmail.com>
2017-07-15 11:43:56 -0400

    question parser

---
+++ parser.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+def questions():
+    f = open('questions.txt', 'rU')
+    s = f.read()
+    qlist = s.split('\n\n')
+
+    for i in range(len(qlist)):
+        q = qlist[i]
+        qlist[i] = q.split('\n')
+    return qlist
