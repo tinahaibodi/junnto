c64b7d17d081a2b65606f81ef72dac1610555432
YOUR NAME <mabedin1@stuy.edu>
2017-07-15 13:52:29 -0400

    added checking of answers and customized responses

--- sender.py
+++ sender.py
@@ -1,4 +1,5 @@
 import random
+ans = 0
 
 def questions():
     f = open('questions.txt', 'rU')
@@ -16,8 +17,20 @@ def generateText():
     string = qlist[x][0] + "\n"
     answers = [1,2,3,4]
     random.shuffle(answers)
-
+    global ans
+    ans = answers.index(1) + 1
+    
+    n = 1
     for e in answers:
-        string += qlist[x][e] + "\n"
+        string += str(n) + ") " + qlist[x][e] + "\n"
+        n += 1
 
     return string
+
+def check(answer):
+    if(answer == ans):
+        return "You are correct!"
+    else:
+        return "Incorrect"
+
+print generateText()
